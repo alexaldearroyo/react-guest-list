bca69d010bde4efe31e9d79b3c58e44ed462be3a
alexaldearroyo <alexaldearroyo@gmail.com>
2024-06-02 23:34:20 +0200

    Add divs with data-test-id=attribute to guest list's items

--- src/components/GuestForm.js
+++ src/components/GuestForm.js
@@ -21,32 +21,38 @@ const GuestForm = ({ addGuest, disabled }) => {
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ mb: 2 }}>
-      <TextField
-        label="First name"
-        value={firstName}
-        onChange={(event) => setFirstName(event.target.value)}
-        placeholder="First name"
-        required
-        fullWidth
-        sx={{ mb: 2 }}
-      />
-      <TextField
-        label="Last name"
-        value={lastName}
-        onChange={(event) => setLastName(event.target.value)}
-        placeholder="Last name"
-        required
-        fullWidth
-        onKeyDown={handleKeyDown}
-        sx={{ mb: 2 }}
-      />
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Button variant="contained" type="submit">
-          Add Guest
-        </Button>
+    <div data-test-id="guest">
+      <Box component="form" onSubmit={handleSubmit} sx={{ mb: 2 }}>
+        <TextField
+          id="first-name"
+          label="First name"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+          placeholder="First name"
+          required
+          fullWidth
+          disabled={disabled}
+          sx={{ mb: 2 }}
+        />
+        <TextField
+          id="last-name"
+          label="Last name"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+          placeholder="Last name"
+          required
+          fullWidth
+          onKeyDown={handleKeyDown}
+          disabled={disabled}
+          sx={{ mb: 2 }}
+        />
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button variant="contained" type="submit" disabled={disabled}>
+            Add Guest
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </div>
   );
 };
 
--- src/components/GuestItem.js
+++ src/components/GuestItem.js
@@ -9,43 +9,42 @@ import {
 import React from 'react';
 
 const GuestItem = ({ guest, removeGuest, toggleAttending }) => (
-  <Box
-    data-test-id="guest"
-    sx={{ mb: 1, display: 'flex', alignItems: 'center' }}
-  >
-    <ListItem sx={{ flex: 1, display: 'flex', alignItems: 'center' }}>
-      <ListItemText
-        primary={
-          <Typography variant="h6">
-            {guest.firstName} {guest.lastName}
-          </Typography>
-        }
-      />
-      <Typography
-        variant="subtitle1"
-        sx={{ mr: 2, color: guest.attending ? 'green' : 'red' }}
-      >
-        Attending: {guest.attending ? 'Yes' : 'No'}
-      </Typography>
-      <Checkbox
-        checked={guest.attending}
-        onChange={() => toggleAttending(guest.id)}
-        aria-label={`${guest.firstName} ${guest.lastName} attending status`}
-        sx={{ mr: 2 }}
-      />
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={() => removeGuest(guest.id)}
-        sx={{
-          backgroundColor: '#E44D2E',
-          '&:hover': { backgroundColor: '#D43A1A' },
-        }}
-      >
-        Remove
-      </Button>
-    </ListItem>
-  </Box>
+  <div data-test-id="guest">
+    <Box sx={{ mb: 1, display: 'flex', alignItems: 'center' }}>
+      <ListItem sx={{ flex: 1, display: 'flex', alignItems: 'center' }}>
+        <ListItemText
+          primary={
+            <Typography variant="h6">
+              {guest.firstName} {guest.lastName}
+            </Typography>
+          }
+        />
+        <Typography
+          variant="subtitle1"
+          sx={{ mr: 2, color: guest.attending ? 'green' : 'red' }}
+        >
+          Attending: {guest.attending ? 'Yes' : 'No'}
+        </Typography>
+        <Checkbox
+          checked={guest.attending}
+          onChange={() => toggleAttending(guest.id)}
+          aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+          sx={{ mr: 2 }}
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => removeGuest(guest.id)}
+          sx={{
+            backgroundColor: '#E44D2E',
+            '&:hover': { backgroundColor: '#D43A1A' },
+          }}
+        >
+          Remove
+        </Button>
+      </ListItem>
+    </Box>
+  </div>
 );
 
 export default GuestItem;
